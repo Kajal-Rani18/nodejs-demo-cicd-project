336519282d1ed5382fc98d4f747b730d8afaa29d
Ubuntu <ubuntu@ip-172-31-19-161.ec2.internal>
2026-06-23 10:56:42 +0000

    Updated server respone

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const http = require('http');
 
 http.createServer((req,res)=>{
     res.end('Hello from Jenkins Docker CI/CD');
-}).listen(3000);
+}).listen(3000, () => {
+	console.log('Server running on port 3000');
+});
