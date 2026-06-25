c4ce3fb760facf0cf54de77ab9777947183c1493
Ubuntu <ubuntu@ip-172-31-19-161.ec2.internal>
2026-06-25 16:25:32 +0000

    Deploy Version 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.end('Hello from Jenkins Docker CI/CD');
+    res.end('Hello from Jenkins Docker CI/CD\n Version 2 deployed automatically');
 }).listen(3000, () => {
 	console.log('Server running on port 3000');
 });
