68112adaf545d9cc5b9187e5cd24958aa7424a88
Ubuntu <ubuntu@ip-172-31-19-161.ec2.internal>
2026-06-26 10:00:23 +0000

    Webhook testing enabled

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.end('Hello from Jenkins Docker CI/CD\n Version 2 deployed automatically');
+    res.end('Hello from Jenkins Docker CI/CD\nVersion 2 deployed automatically\nWebhook Testing for CICD');
 }).listen(3000, () => {
 	console.log('Server running on port 3000');
 });
