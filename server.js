66ea3ca6f88a727367907245174b5cadd2e6cd63
Ubuntu <ubuntu@ip-172-31-19-161.ec2.internal>
2026-06-26 11:17:54 +0000

    Debugging Issue on jenkins

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.end('Hello from Jenkins Docker CI/CD\nVersion 2 deployed automatically\nWebhook Testing for CICD\nChecking finally all looks fine or not');
+    res.end('Hello from Jenkins Docker CI/CD\nTest1.Version 2 deployed automatically\nTest2.Webhook Testing for CICD\ntest3.Checking finally all looks fine or not\nTest5.checking why built in node keeps on offline');
 }).listen(3000, () => {
 	console.log('Server running on port 3000');
 });
